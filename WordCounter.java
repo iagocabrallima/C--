e606d1439eea0eb141e9f72eb99818e3ecd53953
iagol lima <lim22008@byui.edu>
2024-06-27 16:05:51 -0600

    change

--- WordCounter.java
+++ WordCounter.java
@@ -3,20 +3,21 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
+//imports
 
 
 public class WordCounter {
 
     // Function to count words in a document
+    //Receive the file name and return a map with the words count
     public static Map<String, Integer> countWords(String filename) {
         Map<String, Integer> wordCount = new HashMap<>();
-
+        //We use the BufferedReader and Filereader to read the file line by line
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
+            //This part we read the file linbe by line and use split to split in words
             while ((line = br.readLine()) != null) {
                 String[] words = line.split("\\s+");
-
                 for (String word : words) {
                     // Remove punctuation and convert to lowercase
                     word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
@@ -27,6 +28,7 @@ public class WordCounter {
                     }
                 }
             }
+        //We handle exceptions that may occur when opening or reading the file.
         } catch (IOException e) {
             System.err.println("Error opening file: " + e.getMessage());
         }
